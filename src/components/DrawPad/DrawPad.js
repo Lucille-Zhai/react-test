6eb23b09dd3700471edb6aa08ce0fcb86a11ed9f
Lucille-Zhai <1334183421@qq.com>
2018-02-24 23:33:42 +0800

    init

---
+++ src/components/DrawPad/DrawPad.js
@@ -0,0 +1,5 @@
+import React, {Component} from 'react';
+
+export default class DrawPad extends Component {
+    render () {}
+}
---
+++ src/pages/Designer/Designer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class Designer extends Component {
+    render () {
+        return (
+            <div>
+                this is designer~
+            </div>
+        )
+    }
+}
